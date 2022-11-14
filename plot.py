eb2f1070820037da154bac89181fca9f2ea18957
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2022-11-15 03:54:00 +0530

    Update plot.py

--- plot.py
+++ plot.py
@@ -12,16 +12,18 @@ def plot_predictions(predictions):
     for i in range(length):
       try:
         plt.subplots(rows, columns, i+1)
-        plt.bar(['GlaucomaNegative', 'GlaucmaPositive'], predictions[i], color=['#42f557', '#f54242'], label='Negative: predictions[0][0] \nPositive: predictions[0][0]')
-        plt.title(f'image: {i}')
-        plt.ylim(0, 1)
-        plt.legend()
+        fun(predictions[i], i)
       except:
         print('Error: unable to plot image %d'%(i))
                 
   else:
+    fun(predictions[0], 1)
+  
+  def fun(predictions, i):
     plt.bar(['GlaucomaNegative', 'GlaucmaPositive'], predictions[0], color=['#42f557', '#f54242'], label='Negative: predictions[0][0] \nPositive: predictions[0][0]')
-        
+    plt.ylim(0, 1)
+    plt.legend()
+    plt.title(f'image: {i}')
     
   plt.savefig('results.pdf')
   print('Results are saved as result.pdf in same directory')
@@ -34,10 +36,10 @@ def plot_accuracy(history, epochs):
   acc = history.history['accuracy']
   val_acc = history.history['val_accuracy']
     
-  loss = history.history['losss']
+  loss = history.history['loss']
   val_loss = history.history['val_loss']
     
-  epoches_range = range(epochs)
+  epochs_range = range(epochs)
     
   plt.figure(figsize=(8,8))
   plt.subplot(1, 2, 1)
@@ -51,7 +53,7 @@ def plot_accuracy(history, epochs):
   plt.plot(epochs_range, val_loss, label='Validation loss')
   plt.legend()
   plt.title('Training and Validation loss')
-    
+  
   plt.show()
     
 def plot_samples():

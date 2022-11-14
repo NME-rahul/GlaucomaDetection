5cae3f4ad106cbdb7ea16c24a2e9161196c72045
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2022-11-15 03:53:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #user defined modules
 import plot
-import train_model
+import train_model as tm
 import image_preprocess
 
 #pre-defined modules
@@ -10,47 +10,59 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Model
+from tensorflow.keras.applications.resnet50 import decode_predictions
 
-def start():
-  if len(sys.argv) > 2:
-    sys.exit("\nError: main.py except only 1 argument 'trai_model'")
-  elif len(sys.argv) == 2 and sys.argv[1] == 'train_model':
-    train_model.load_data()
-    model = train_model.train()
-    if model == False:
-      sys.exit('\nError: model not found make sure the model is present inside the current working directory')
-    else:
-      train_model.fit_model(model)
-  elif len(sys.argv) == 1 or sys.argv[1] == 'dflt':
-    if os.path.exists('GlaucomaDetection.h5') == True:
-      path = 'GlaucomaDetection.h5'
-    else:
-      path = input('\nModel not found in current working directory \n\nEnter path of model directory: ')
-      if os.path.exists(path+'GlaucomaDetection.h5') == False:
-          print('\nError: Model not found')
-    try:
-      model = tf.keras.models.load_model(path+'GlaucomaDetection.h5')
-    except:
-      sys.exit('\nError: unable to load model')
+#available Error types in this file
+Error1 = '\nError: model not found make sure the model is present inside the current working directory'
+Error2 = '\nError: Courrupt image or size is not 300x300x3'
+Error3 = '\nError: unable to fetch images'
 
-
+def start_predictions(model):
   path = input('\nEnter path of image/s: ')
-  if os.path.isdir() == True:
+  if os.path.isdir(path) == True:
     for image_Path in range(len(os.listdir(path))):
       image_instance = image_preprocess.resize_(image_Path) #resize_ function is defined in image_resize.py
       try:
         predictions = model.predict(image_instance)
         predictions.append(predictions)
       except:
-        print('\nError: unable to fetch images')
+        print(Error3)
         
   else:
     image_instance = image_preprocess.resize_(path)
     try:
       predictions = model.predict(image_instance)
     except:
-      print('\nError: Courrupt image or size is not 300x300x3')
+      print(Error2)
 
   plot.plot_predictions(predictions) #plot_ function is defined in plot.py
 
-start()
+def start():
+  if len(sys.argv) > 3:
+    sys.exit("\nError: main.py except only 1 argument 'train_model'")
+  elif len(sys.argv) == 2 and sys.argv[1] == 'train_model':
+    tm.load_data()
+    tm.create_data()
+    tm.create_generator()
+    if len(sys.argv) == 3 and sys.argv[2] == 'existing':
+      model = tm.load_existing_model()
+    else:
+      model = tm.create_model_ResNet50()
+      model = tm.compile_model(model)
+
+    if model == False:
+      sys.exit(Error1)
+    else:
+      tm.fit_model(model)
+      model.save('trained_model_GlaucomaDetecton.h5')
+    return model
+
+  elif len(sys.argv) == 1 or sys.argv[1] == 'make_predictions':
+    model = tm.load_existing_model()
+    if model == False:
+      sys.exit(Error1)
+    return model
+
+model = start()
+if input("\npress 'P' for making predictions: ") == 'P':
+  start_predictions(model)

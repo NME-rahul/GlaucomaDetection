f6fbc637f8b4cf8451455aa5c9f0f8ce2be569a7
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2023-07-18 21:07:28 +0530

    Update image_preprocess.py

--- image_preprocess.py
+++ image_preprocess.py
@@ -1,18 +1,21 @@
 import cv2 as cv
-import glob
 import os
 
 dflt_img_height = 300
 dflt_img_width = 300
 
-Error3 = '\nError: unable to fetch images or Courrupt image or size is not 300x300x3'
+Error3 = 'Error: unable to fetch images or Courrupt image or size is not 300x300x3\n'
 
 #function which gets the image path from main.py and resize it according model requirments
-def resize_(path):
-  img = cv.imread(path)
+def resize_(image_path):
+  if type(image_path) == list:
+    for i in image_path:
+      resize_(i)
+
+  img = cv.imread(str(image_path))
   try:
     image_instance = cv.resize(img, (dflt_img_height, dflt_img_width))
-    print('Image: ', path)
+    print('Image: ', image_path)
   except:
     print(Error3)
     image_instance = False
@@ -20,14 +23,14 @@ def resize_(path):
   return image_instance
 
 
-def adaptive_hist_flattening(path):
-  if os.path.isdir(path) == True:
-    image_path = glob.glob(path + '/**/*.png', recursive=True)
+def adaptive_hist_flattening(image_path):
+  if type(image_path) == list:
     for i in image_path:
       adaptive_hist_flattening(i)
-  
+
+  print('loading Image...\n ',image_path)
+  img = cv.imread(str(image_path))
   try:
-    img = cv.imread(path)
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     h,s,v = cv.split(hsv)
     clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(3,3))
@@ -39,5 +42,3 @@ def adaptive_hist_flattening(path):
     cv.imwrite(path, rgb)
   except:
     print(Error3)
-    pass
-  print('Image: ',path)
